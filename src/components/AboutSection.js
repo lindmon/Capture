dec1033b4e64157e8bab50fabac25b72948cfce7
lindmon <m.eryzhenskaia@gmail.com>
2023-07-25 21:08:06 +0200

    services section styled, fonts added

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,39 +1,34 @@
 import React from 'react';
 import home1 from '../img/home1.png';
-import styled from 'styled-components';
+// import styled from 'styled-components';
+import{About, Description, Image, Hide} from "../styles"; 
 
 const AboutSection = () => {
     return(
         <About>
-            <div className="description">
+            <Description>
                 <div className="title">
-                   <div className="hide">
+                   <Hide>
                         <h2>We work to make</h2>
-                   </div>
-                   <div className="hide">
+                   </Hide>
+                   <Hide>
                         <h2>your <span>dreams</span> come </h2>
-                   </div>
-                   <div className="hide">
+                   </Hide>
+                   <Hide>
                         <h2>true</h2>
-                   </div>
+                   </Hide>
                 </div>
                 <p>Contact us for any photography or videography issead that you have. We have professionals with amazing skills </p>
                 <button>Contact US</button>
-            </div>
-            <div className="image">
+            </Description>
+            <Image>
                 <img src={home1} alt="guy with the camera" />
-            </div>
+            </Image>
         </About>
     )
 }
 
-const About = styled.div`
-    min-height: 90vh;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    padding: 5rem 10rem;
-    color: white;
-`;
+
+
 
 export default AboutSection;
