13a3a71d7f347eeab3109b8fd3267a996e95e531
lindmon <m.eryzhenskaia@gmail.com>
2023-07-29 00:39:29 +0200

    routing added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import AboutUs from './pages/AboutUs';
+import ContactUs from './pages/ContactUs';
+import OurWork from './pages/OurWork';
 import GlobalStyle from './components/GlobalStyle';
+import Nav from './components/Nav';
+import{Route, Routes} from "react-router-dom";
+//Create the router
+
+
+
+
+//Start the router
 function App() {
   return (
     <div className="App">
-      <GlobalStyle/>
-      <AboutUs/>
+        <GlobalStyle/>
+        <Nav/>
+        <Routes>
+          <Route path="/" element={<AboutUs/>}/>
+          <Route path="/work" element={<OurWork/>}/>
+          <Route path="/contact" element={<ContactUs/>}/>
+
+        </Routes>
     
     </div>
   );
