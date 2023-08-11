830e57a2dd57c36baa2737711eec516a08c76666
lindmon <m.eryzhenskaia@gmail.com>
2023-08-12 00:02:01 +0200

    line animation added

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -2,15 +2,17 @@ import React from 'react';
 import home1 from '../img/home1.png';
 import{About, Description, Image, Hide} from "../styles"; 
 import {motion} from "framer-motion";
+import {titleAnim, fade, photoAnim} from '../animation';
+import Wave from './Wave';
 
 const AboutSection = () => {
-const  titleAnim = {
-    hidden: {opacity:0},
-    show:{opacity:1, transition:{duration:2}}
-};
+// const  titleAnim = {
+//     hidden: {opacity:0},
+//     show:{opacity:1, transition:{duration:2}}
+// };
 const container = {
     hidden:{x:100},
-    show:{x:0, transition:{duration:0.75, ease: "easeOut", staggerChildren:1}}
+    show:{x:0, transition:{duration:0.75, ease: "easeOut", staggerChildren:0.25}}
 };
 
     return(
@@ -27,12 +29,16 @@ const container = {
                         <motion.h2 variants={titleAnim}  >true</motion.h2>
                    </Hide>
                 </motion.div>
-                <p>Contact us for any photography or videography issead that you have. We have professionals with amazing skills </p>
-                <button>Contact US</button>
+                <motion.p variants={fade}>Contact us for any photography or videography 
+                    issead that you have. We have professionals with amazing skills 
+                </motion.p>
+                <motion.button variants={fade}>Contact US</motion.button>
             </Description>
             <Image>
-                <img src={home1} alt="guy with the camera" />
+                <motion.img variants={photoAnim} src={home1} alt="guy with the camera" />
             </Image>
+            <Wave/>
+
         </About>
     )
 }
