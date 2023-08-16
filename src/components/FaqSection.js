43966977f2848e885de3ae70e5ba7a5c5c18ee5e
lindmon <m.eryzhenskaia@gmail.com>
2023-08-16 20:15:22 +0200

    toggle added

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,46 +1,41 @@
 import React from "react";
 import styled from 'styled-components';
 import {About} from '../styles';
+import Toggle from './Toggle';
+import { LayoutGroup} from "framer-motion";
 
 const FaqSection = () => {
     return(
         <Faq>
             <h2>Any questions? <span>FAQ</span></h2>
-            <div className="question">
-                <h4>How do I start ?</h4>
-                <div className="answer">
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat, animi.</p>
-                </div>
-                <div className="faq-line"></div>
-            </div>
-            <div className="question">
-                <h4>Daily schedule</h4>
-                <div className="answer">
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat, animi.</p>
-                </div>
-                <div className="faq-line"></div>
-            </div>
-            <div className="question">
-                <h4>Different payment methods</h4>
-                <div className="answer">
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat, animi.</p>
-                </div>
-                <div className="faq-line"></div>
-            </div>
-            <div className="question">
-                <h4>What products do you offer?</h4>
-                <div className="answer">
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat, animi.</p>
-                </div>
-                <div className="faq-line"></div>
-            </div>
-
+                <LayoutGroup>
+                    <Toggle title="How do I start ?">
+                            <div className="answer">
+                                <p>Lorem ipsum dolor sit amet.</p>
+                                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat, animi.</p>
+                            </div>
+                            
+                    </Toggle>
+                    <Toggle title="Daily schedule">
+                            <div className="answer">
+                                <p>Lorem ipsum dolor sit amet.</p>
+                                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat, animi.</p>
+                            </div>
+                    </Toggle>
+                    <Toggle title="Different payment methods">
+                            <div className="answer">
+                                <p>Lorem ipsum dolor sit amet.</p>
+                                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat, animi.</p>
+                            </div>
+                    </Toggle>
+                    <Toggle title="What products do you offer?">
+                            <div className="answer">
+                                <p>Lorem ipsum dolor sit amet.</p>
+                                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat, animi.</p>
+                            </div>
+                    </Toggle>
+                </LayoutGroup>
         </Faq>
-
     )
 }
 
